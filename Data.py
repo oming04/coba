6c4b2948a902a446144b9a3319801369584f9981
oming04 <96978897+oming04@users.noreply.github.com>
2023-05-06 00:04:32 +0800

    Update Data.py

--- Data.py
+++ Data.py
@@ -9,20 +9,8 @@ class Data:
 <b> ❏ Perintah untuk Pengguna BOT
  ├ /start - Mulai Bot
  ├ /about - Tentang Bot ini
- ├ /help - Bantuan Perintah Bot ini
  ├ /ping - Untuk mengecek bot hidup
  └ /uptime - Untuk melihat status bot 
- 
- ❏ Perintah Untuk Admin BOT
- ├ /logs - Untuk melihat logs bot
- ├ /setvar - Untuk mengatur var dengan command dibot
- ├ /delvar - Untuk menghapus var dengan command dibot
- ├ /getvar - Untuk melihat salah satu var dengan command dibot
- ├ /users - Untuk melihat statistik pengguna bot
- ├ /batch - Untuk membuat link lebih dari satu file
- ├ /speedtest - Untuk Mengetes kecepatan server bot
- └ /broadcast - Untuk mengirim pesan broadcast ke pengguna bot
-
 👨‍💻 Develoved by </b><a href='https://t.me/Lunatic0de/101'>@Lunatic0de</a>
 """
 
